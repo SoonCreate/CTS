ba8c200623f10707f11940669e48daf11f7b7221
yacole <zky.yacole@gmail.com>
2014-10-04 10:55:48 +0800

    file_model，重新add

---
+++ application/models/file_model.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class File_model extends MY_Model{
+
+    function __construct(){
+        parent::__construct();
+        //设置钩子
+        $this->before_create = array('before_insert');
+        $this->before_update = array('before_update');
+    }
+
+    function before_insert($data){
+        return set_creation_date($data);
+    }
+
+    function before_update($data){
+        return set_last_update($data);
+    }
+
+}
