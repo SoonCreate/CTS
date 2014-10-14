0e940162d2747688c7d30cb0c41959940df0805c
gs1357 <gs1357@163.com>
2014-10-14 10:13:34 +0800

    个人资料修改页面

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -1,8 +1,20 @@
-<h1>用户信息更新</h1>
+<div class="row paneltitle">
+    <h3>用户信息更新</h3>
+</div>
 <form id="user_edit" method="post" action="<?= _url('user',_v('to'))?>">
-    <label for="username">*用户名</label>
-    <input name="username" id="username" type="text" value="<?= _v('username')?>" disabled/><br/>
-    <input name="id" id="id" type="hidden" value="<?= _v('id')?>" />
+<div class="container-fluid userd">
+    <dl class="row dl-horizontal">
+        <dt><label for="username">*用户名</label></dt>
+        <dd>
+            <input name="username" id="username" type="text" value="<?= _v('username')?>"  data-dojo-type="sckj/form/TextBox" disabled/>
+            <input name="id" id="id" type="hidden" value="<?= _v('id')?>" />
+        </dd>
+    </dl>
     <?php $this->load->view('user/_form') ;?>
-    <button type="submit">提交</button>
+
+</div>
+
+<div class="row panelbottom">
+        <button type="submit" data-dojo-type="sckj/form/Button" class="success">提交</button>
+</div>
 </form>
