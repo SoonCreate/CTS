354f754ef7d2e7839a088262c9443380395929bd
gs1357 <gs1357@163.com>
2014-10-15 00:43:48 +0800

    我的功能 页面及相关CSS优化

--- application/views/my_functions.php
+++ application/views/my_functions.php
@@ -1,9 +1,14 @@
-<h1>我的功能</h1>
+<div class="headline">
+    <h2>我的功能</h2>
+</div>
 <?php if(isset($functions)):?>
-    <ul>
+    <ul class="applist row">
         <?php  foreach($functions as $fn) :?>
             <li>
+                <div class="icon"><i class="icon-tasks icon-3x"></i></div>
+                <div class="text">
                 <?php render_link(_url('welcome','go',array('cm'=>$fn['id'])),$fn['function_desc'])?>
+                </div>
             </li>
         <?php  endforeach;?>
     </ul>
