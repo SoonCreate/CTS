f06ae79da41d214ddbf051bc2f5be3c68f040cba
yacole <zky.yacole@gmail.com>
2014-11-07 09:27:59 +0800

    a little fix

--- application/views/status_condition/object_item_edit.php
+++ application/views/status_condition/object_item_edit.php
@@ -1,14 +1,8 @@
 <?= render_form_header('item_edit');?>
 <div class="container-fluid userd">
     <?= render_form_open('status_condition','object_item_edit') ?>
-    <?= render_form_input('auth_value',true)?>
-    <?= render_button('s','_selectDialog(\''._v('auth_item_name').'\')')?>
+    <?= render_form_input_vl('auth_value',_v('auth_item_name'),true,false,true,true,false)?>
     <?= render_form_hidden('id',v('id'));?>
     <?= render_button_group();?>
     <?= render_form_close() ?>
 </div>
-<script type="text/javascript">
-    function _selectDialog(name){
-        vlGridDialog(name,null,true,true,dijitObject('auth_value'),false);
-    }
-</script>
