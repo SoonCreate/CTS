8ab3e0f5f92a3d2c95d6ccfd0f450e423b86333b
yacole <zky.yacole@gmail.com>
2014-11-20 08:36:03 +0800

    message的help帮助功能：点击消息弹出帮助文档

--- application/helpers/handle_data_helper.php
+++ application/helpers/handle_data_helper.php
@@ -18,6 +18,7 @@ function message($type,$class_code,$message_code,$args = array()){
         $data['type'] = $type;
         $data['code'] = $class_code.'('.$message_code.')';
         $data['content'] = $message['content'];
+        $data['help'] = $message['help'];
         //处理内容
         if(count($args) > 0){
             $content = $message['content'];
@@ -58,7 +59,8 @@ function ci_translate_message($type,$line){
 
 //数据库操作成功
 function message_db_success(){
-    ci_translate_message('I','message_save_success');
+    message('I','db','10');
+//    ci_translate_message('I','message_save_success');
 }
 //没有权限
 function message_no_authority(){
