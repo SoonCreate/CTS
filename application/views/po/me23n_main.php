c4dccd3b8c2a2c527836c39710f24996f0369142
yacole <zky.yacole@gmail.com>
2015-06-29 16:57:58 +0800

    页面调整

--- application/views/po/me23n_main.php
+++ application/views/po/me23n_main.php
@@ -1,16 +1,27 @@
-<h2>采购订单详情</h2>
+<?= render_form_header('po_header') ?>
 <?php if(isset($PO_HEADER)){?>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-4">
-                供应商编号:<?= $PO_HEADER['VENDOR'] ?>
-            </div>
-            <div class="col-md-4">
-                供应商名称:<?= $PO_HEADER['VEND_NAME'] ?>
-            </div>
-            <div class="col-md-4">
-                订单号:<?= $PO_HEADER['PO_NUMBER'] ?>
-            </div>
-        </div>
+    <div class="container-fluid userd">
+        <dl class="row dl-horizontal">
+            <dt><label>供应商编号</label></dt>
+            <dd><?= $PO_HEADER['VENDOR'] ?></dd>
+            <dt><label>供应商名称</label></dt>
+            <dd><?= $PO_HEADER['VEND_NAME'] ?></dd>
+        </dl>
+        <dl class="row dl-horizontal">
+            <dt><label>订单号</label></dt>
+            <dd><?= $PO_HEADER['PO_NUMBER'] ?></dd>
+        </dl>
     </div>
+    <table class="container-fluid userd">
+        <tr class="row dl-horizontal">
+            <td><label>供应商编号</label></td>
+            <td><?= $PO_HEADER['VENDOR'] ?></td>
+            <td><label>供应商名称</label></td>
+            <td><?= $PO_HEADER['VEND_NAME'] ?></td>
+        </tr>
+        <tr class="row dl-horizontal">
+            <td><label>订单号</label></td>
+            <td><?= $PO_HEADER['PO_NUMBER'] ?></td>
+        </tr>
+    </table>
 <?php }?>
