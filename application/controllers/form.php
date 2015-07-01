8dd56ff1156055aee52ef6849c8c8b01b7669f15
Administrator <zky.yacole@gmail.com>
2015-07-01 21:12:42 +0800

    开始设计表单字段管理

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -5,9 +5,54 @@ class Form extends CI_Controller {
     function __construct(){
         parent::__construct();
         header('Content-Type: text/html; charset=utf-8');
+        $this->load->model('form_group_model');
+        $this->load->model('form_model');
     }
 
     public function index()
     {
+        $fm = new Form_model();
     }
+
+    function form_tree(){
+
+    }
+
+    function create(){
+
+    }
+
+    function edit(){
+
+    }
+
+    function destroy(){
+
+    }
+
+    function form_group(){
+
+    }
+
+    function form_group_create(){
+
+    }
+
+    function form_group_edit(){
+
+    }
+
+    function form_group_destroy(){
+
+    }
+
+    function fields(){
+
+    }
+
+    function field_update(){
+
+    }
+
+
 }
